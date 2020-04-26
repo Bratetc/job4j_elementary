a02628761538b93603a8435a4b24405be6c50751
Bratetc <don.Sebastian.Pereira@mail.ru>
2020-04-26 21:18:22 +0800

    8. Стороны прямоугольника.[#278703]

---
+++ src/ru/job4j/condition/SqArea.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class SqArea {
+    public static double square(int p, int k){
+        double rsl = Math.pow(p/(2*(k+1)),2) * k;
+        return rsl;
+    }
+
+    public static void main(String[] args){
+        double result1 = SqArea.square(6,2);
+        System.out.println("The area of a rectangle with a perimeter p = 6 and the coefficient k = 2 is equal to " + result1);
+    }
+}
