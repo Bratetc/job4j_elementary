0f7689346793f15c1909932a89fb50058543ea7b
Bratetc <don.Sebastian.Pereira@mail.ru>
2020-04-26 21:34:22 +0800

    9. Площадь треугольника.[#278706]

---
+++ src/ru/job4j/condition/TrgArea.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+public class TrgArea {
+
+    public static double area(double a, double b, double c){
+        double p = (a+b+c)/2; // Полупериметр
+        double rsl = Math.sqrt(p*(p-a)*(p-b)*(p-c));
+        return rsl;
+    }
+
+    public static void main(String[] args){
+        double rsl = TrgArea.area(2,2,2);
+        System.out.println("The area of a triangle with sides 2, 2, 2 is equal to " + rsl);
+    }
+}
