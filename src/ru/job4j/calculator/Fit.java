8aef49ed33857223eab42ba36da61b7b99125688
Alexey Shabanov <don.Sebastian.Pereira@mail.ru>
2020-04-27 20:39:16 +0800

    Добавлены пробелы между ) и {

--- src/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/calculator/Fit.java
@@ -2,17 +2,17 @@ package ru.job4j.calculator;
 
 public class Fit {
 
-    public static double manWeight(double height){
+    public static double manWeight(double height) {
         //Идеальный вес для мужчин = (рост в сантиметрах – 100) · 1,15
         double rsl = (height - 100) * 1.15;
         return rsl;
     }
-    public static double womanWeight(double height){
+    public static double womanWeight(double height) {
         //Идеальный вес для женщин = (рост в сантиметрах – 110) · 1,15.
         double rsl = (height - 110) * 1.15;
         return rsl;
     }
-    public static void main(String[] args){
+    public static void main(String[] args) {
         double man = Fit.manWeight(100);
         double woman = Fit.womanWeight(100);
         System.out.println("Man 100 is " + man);
