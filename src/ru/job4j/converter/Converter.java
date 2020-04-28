6cc531435700579560e661cab652fa7298923dbc
Bratetc <don.Sebastian.Pereira@mail.ru>
2020-04-28 21:54:51 +0800

    Оставил вариант с Int

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -2,25 +2,25 @@ package ru.job4j.converter;
 
 public class Converter {
 
-    public static int rubleToEuro(int value){
+    public static int rubleToEuro(int value) {
         //Необходимо изменить тип с int на double
         return value / 70;
     }
 
-    public static int rubleToDollar(int value){
+    public static int rubleToDollar(int value) {
         //Необходимо изменить тип с int на double
         return value / 60;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int in = 140;
-        double expected = 2;
+        int expected = 2;
         int out = rubleToEuro(140);
         boolean passed = expected == out;
         System.out.println("140 rubles are 2 euro. Test result : " + passed);
-        expected = 2.33;
+        expected = 2;
         out = Converter.rubleToDollar(140);
         passed = expected == out;
-        System.out.println("140 rubles are 2.33 dollars. Test result : " + passed);
+        System.out.println("140 rubles are 2 dollars. Test result : " + passed);
     }
 }
