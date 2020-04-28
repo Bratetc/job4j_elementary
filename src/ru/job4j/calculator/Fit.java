8a72e9583b841084f421190907c91de268c74127
Bratetc <don.Sebastian.Pereira@mail.ru>
2020-04-28 21:37:00 +0800

    Указал средний рост мужчины и женщины. Как по мне, так в результате работы программы идеальный вес для средних мужчины и женщины далёк от идеального.

--- src/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/calculator/Fit.java
@@ -2,20 +2,20 @@ package ru.job4j.calculator;
 
 public class Fit {
 
-    public static double manWeight(double height){
+    public static double manWeight(double height) {
         //Идеальный вес для мужчин = (рост в сантиметрах – 100) · 1,15
         double rsl = (height - 100) * 1.15;
         return rsl;
     }
-    public static double womanWeight(double height){
+    public static double womanWeight(double height) {
         //Идеальный вес для женщин = (рост в сантиметрах – 110) · 1,15.
         double rsl = (height - 110) * 1.15;
         return rsl;
     }
-    public static void main(String[] args){
-        double man = Fit.manWeight(100);
-        double woman = Fit.womanWeight(100);
-        System.out.println("Man 100 is " + man);
-        System.out.println("Woman 100 is " + woman);
+    public static void main(String[] args) {
+        double man = Fit.manWeight(178);
+        double woman = Fit.womanWeight(166);
+        System.out.println("Man 178 is " + man);
+        System.out.println("Woman 166 is " + woman);
     }
 }
